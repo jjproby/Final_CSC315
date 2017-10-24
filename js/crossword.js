f43626e1d0361eeaa5f18426d14e9d281d812f75
jjproby <jjproby719@gmail.com>
2017-10-24 08:59:10 -0500

    start of jquery file and start of js file

--- js/crossword.js
+++ js/crossword.js
@@ -1,1 +1,29 @@
 //javascript for crossword puzzle
+
+(function($) {
+  $(function() {
+    //the values for the crossword puzzle clues
+
+    var puzzleData = [
+      {
+        clue: "First letter of greek alphabet",
+        answer: "alpha",
+        position: 1,
+        orientation: "across",
+        startx: 1,
+        starty: 1,
+      },
+      {
+        clue: "Defines the alpha-numeric characters that are typically associated with text used in programming",
+        answer: "ascii",
+        position: 1,
+        orientation: "down",
+        startx: 1,
+        starty: 1
+      }
+    ]
+
+    $('#puzzle-wrapper').crossword(puzzleData);
+
+  })
+})(jQuery)
--- js/jquery.crossword.js
+++ js/jquery.crossword.js
@@ -1,1 +1,60 @@
 // Jquery stuff for crossword puzzle
+
+
+
+(function($)){
+  $.fn.crossword = function(entryData) {
+
+    var puzzle = {};
+    puzzle.data = entryData;
+
+
+    this.after('<div id="puzzle-clues"><h2>Across</h2><ol id="across"></o1><h2>Down</h2><ol id="down"></ol></div>');
+
+    var tb1 = ['<table id = "puzzle">'];
+    var puzzleE1 = this;
+    var clues = $('#puzzle-clues');
+    var clueListE1;
+    var coords;
+    var entryCount = puzzle.data.length;
+    var entries = [];
+    var rows = [];
+    var cols = [];
+    var solved = [];
+    var tabindex;
+    var $actives;
+    var activePosition = 0;
+    var activeClueIndex = 0;
+    var currentOri;
+    var targetInput;
+    var mode = 'interacting';
+    var solvedToggle = false;
+    var z = 0;
+
+    var puzInit = {
+
+      init: function() {
+        currentOri = 'across' //initial orientation
+
+        puzzle.data.sort(function(a,b) {
+          return a.position - b.position;
+        });
+
+        puzzleE1.delegate('input', 'keyup', function(e) {
+          mode = 'interacting';
+
+          switch (e.which) {
+            case 39:
+            case 37:
+              currentOri
+
+              break;
+            default:
+
+          }
+        })
+      }
+    }
+
+  }
+ }
